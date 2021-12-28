2e3a43d0ccd60150be5f443f66d73e170c7af33d
Leonardo Mayoral <mayoral.leonardo99@gmail.com>
2021-12-27 23:31:50 -0300

    finished movie details page

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,22 +1,32 @@
 import { useEffect, useState } from 'react';
 import './filme-info.css';
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import api from '../../services/api';
 
 export default function Filme() {
   const { id } = useParams();
+  const history = useHistory();
   const [filme, setFilme] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadFilme() {
       const response = await api.get(`r-api/?api=filmes/${id}`);
-      // console.log(response.data);
+
+      if (response.data.length === 0) {
+        history.replace('/');
+        return;
+      }
+
       setFilme(response.data);
       setLoading(false);
     }
     loadFilme();
-  }, [id]);
+
+    return () => {
+
+    }
+  }, [history, id]);
 
   if (loading) {
     return (
@@ -27,7 +37,20 @@ export default function Filme() {
   }
   return (
     <div className="filme-info">
-      <h1>Página Detalhes - {id}</h1>
+      <h1>{filme.nome}</h1>
+      <img src={filme.foto} alt={filme.nome} />
+
+      <h3>Sinopse</h3>
+      {filme.sinopse}
+
+      <div className='botoes'>
+        <button onClick={() => { }}>Salvar</button>
+        <button onClick={() => { }}>
+          <a target="blank" href={`https://youtube.com/results?search_query=${filme.nome} Trailer`}>
+          Trailer
+          </a>
+        </button>
+      </div>
     </div>
   )
 }
